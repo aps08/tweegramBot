0e58a004d35fcb1d062b352671a1cb626562bdfb
aps08 <anoopprsingh.social@gmail.com>
2022-09-27 00:18:58 +0530

    read text from tel

---
+++ src/helper/__init__.py
@@ -0,0 +1,2 @@
+from helper.telegram import RecieveMessage
+from helper.twitter import SendTweet
---
+++ src/helper/twitter.py
@@ -0,0 +1,8 @@
+"""
+This module is responsible for converting messages
+and media to tweet received from telegram.
+"""
+
+
+class SendTweet:
+    pass
