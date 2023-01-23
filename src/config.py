601af3f2b85518ee424d3d187ced379e6b58cbae
aps08 <anoopprsingh.social@gmail.com>
2023-01-23 23:52:44 +0530

    account setup done

--- src/config.py
+++ src/config.py
@@ -4,9 +4,11 @@ All id, key and token for twitter and telegram.
 import os
 
 # comment added
-TEL_API_ID = os.environ.get("TEL_API_ID")
-TEL_API_HASH = os.environ.get("TEL_API_HASH")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+
 TEL_GROUP = os.environ.get("TEL_GROUP")
+
 TWT_API_KEY = os.environ.get("TWT_API_KEY")
 TWT_API_SECRET = os.environ.get("TWT_API_SECRET")
 TWT_ACCESS_TOKEN = os.environ.get("TWT_ACCESS_TOKEN")
