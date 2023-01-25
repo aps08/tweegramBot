f25c9bfadce78cc75aeb3cb48198d3d79d6af40c
aps08 <anoopprsingh.social@gmail.com>
2023-01-26 01:02:04 +0530

    worked on command and get message function

--- src/helper/destination.py
+++ src/helper/destination.py
@@ -1,3 +1,37 @@
 """
 All twitter operations are performed here.
 """
+
+
+class TwitterOps:
+    """
+    Destination Operations
+    """
+
+    def __init__(self):
+        """construtor"""
+        pass
+
+    def user_lookup(self, username: str) -> bool:
+        """
+        returns True if username exists on Twitter
+        """
+        exists = False
+        try:
+            pass
+        except Exception as loopup_err:
+            raise loopup_err
+        return exists
+
+    def send_dm(self, token: str) -> None:
+        """
+        send direct message with token
+        to be used for tweets.
+        """
+        try:
+            pass
+        except Exception as dm_err:
+            raise dm_err
+
+    def tweet(self) -> None:
+        pass
--- src/helper/source.py
+++ src/helper/source.py
@@ -3,7 +3,9 @@ All telegram operations are performed here.
 """
 
 import os
+import re
 from datetime import datetime, timedelta, timezone
+from typing import Tuple
 
 from dotenv import load_dotenv
 from telethon.sync import TelegramClient
@@ -11,7 +13,7 @@ from telethon.sync import TelegramClient
 load_dotenv()
 
 
-class TelegramSourceOps:
+class TelegramOps:
     """
     Source operations
     """
@@ -20,26 +22,43 @@ class TelegramSourceOps:
         """constructor"""
         self.__client = TelegramClient("aps08", API_ID, API_HASH).start()
 
-    def __command_check(self):
-        """
-        Checks for all commands added.
-        """
-        try:
-            pass
-        except Exception as command_err:
-            raise command_err
-
-    def get_messages(self):
+    def get_messages(self) -> Tuple[list, list]:
         """
         Gets all the messages which are posted
         after a perticular interval of time.
         """
+        fetched_data = []
+        command_message = []
         try:
             date_time = datetime.now(timezone.utc) - timedelta(hours=1.0)
-            for message in self.__client.iter_messages("pytweegram", offset_date=date_time):
-                print(message)
+            for message in self.__client.iter_messages(
+                entity="pytweegram", offset_date=date_time, reverse=True
+            ):
+                current_timestamp = datetime.now().strftime("%Y%d%m_%H%M%S_%f")
+                export = "media/" + current_timestamp
+                fetched_message, image_path = None, None
+                if (message.photo and message.message) and ("@notice" not in message.message):
+                    image_path = self.__client.download_media(message, export)
+                elif message.photo and not message.message:
+                    image_path = self.__client.download_media(message, export)
+                elif (message.message and not message.photo) and ("@notice" not in message.message):
+                    text = message.message
+                    text = text.strip()
+                    if re.match("@add", text, re.IGNORECASE) or re.match(
+                        "@remove", text, re.IGNORECASE
+                    ):
+                        command_message.append(text)
+                    else:
+                        fetched_message = text
+                else:
+                    print("Notice chat/Not text or image.")
+                if image_path and "\\" in image_path:
+                    image_path = image_path.replace("\\", "/")
+                if fetched_message or image_path:
+                    fetched_data.append({"message": fetched_message, "image": image_path})
         except Exception as get_message_err:
             raise get_message_err
+        return fetched_data, command_message
 
     def send_message(self, text: str):
         """
@@ -47,10 +66,10 @@ class TelegramSourceOps:
         account user is trying to add doesn't exists.
         """
         try:
-            pass
+            self.__client.send_message(entity="me", message=text)
         except Exception as send_message_err:
             raise send_message_err
 
 
-TelegramSourceOps = TelegramSourceOps(os.environ.get("API_ID"), os.environ.get("API_HASH"))
-TelegramSourceOps.get_messages()
+TelegramOps = TelegramOps(os.environ.get("API_ID"), os.environ.get("API_HASH"))
+print(TelegramOps.get_messages())
